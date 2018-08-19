3f252e4d8d1a23499ad05529ed03d99718a18b2f
Cain O'Sullivan <cain.osullivan@gmail.com>
2018-08-19 19:35:56 +0800

    WIP

--- CSharp/GrowthZone.Shared/GrowthZoneClient.cs
+++ CSharp/GrowthZone.Shared/GrowthZoneClient.cs
@@ -8,10 +8,9 @@ namespace GrowthZone.Shared
 {
     public sealed class GrowthZoneClient : IDisposable
     {
-        //public const string Host = "https://tardisenterprises.gz-10958.growthzonebranch.com";
         public const string Host = "https://growthzone.growthzonedev.com";
-        public const string ClientId = "OG46DpRN3j0dnD3os9debN0djKXNllI6VH73jfaGRtI";
-        public const string ClientSecret = "GeD13PT6MsdcHhD80kX0Vw";
+        public const string ClientId = "<client id>";
+        public const string ClientSecret = "<client secret>";
 
         //public const string Host = "http://localtest.me:12221";
         //public const string ClientId = "123ABC";
