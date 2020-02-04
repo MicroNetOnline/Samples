933338d294d33e0c63284b245d0b87628c0a5dc0
Cain O'Sullivan <cain.osullivan@gmail.com>
2020-02-04 22:55:26 +0800

    Update GrowthZoneClient.cs

--- CSharp/GrowthZone.Shared/GrowthZoneClient.cs
+++ CSharp/GrowthZone.Shared/GrowthZoneClient.cs
@@ -16,10 +16,6 @@ namespace GrowthZone.Shared
         public const string ClientId = "MnW6ar1wXwbVpm5LVzuTJg";
         public const string ClientSecret = "6Cfi2JwUDq1FfeiRzUiYBfuno1IWuMqiUfQkkPqqz4";
 
-        //public const string Host = "https://growthzone.growthzoneapp.com";
-        //public const string ClientId = "KqhPqaHocxoVxw1S9cIfwanUR10vcCYuxJWl907Tg";
-        //public const string ClientSecret = "mzpNMa5F7pX6XI9mAxG2FQ";
-
         readonly HttpClient _httpClient;
 
         /// <summary>
